1fd81e6b11817837424fb47d9647265229021963
Edson Silva <edson.junior@ufpi.edu.br>
2024-07-10 22:46:38 -0300

    Project created

---
+++ src/test/java/course/spring/Spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package course.spring.Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
